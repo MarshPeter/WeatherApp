c46cd7ad80929f03183a29072bc22f66b4bb9340
Peter <peter.m.farmer.204@gmail.com>
2023-04-04 00:42:45 +1000

    fetch working

---
+++ app.js
@@ -0,0 +1,17 @@
+// weather API:
+// b4beec06fb894741986141820230304
+
+// TODO: Set these to environment variables
+const API_KEY = "b4beec06fb894741986141820230304";
+const BASE_URL = "http://api.weatherapi.com/v1";
+
+console.log("Hello World");
+
+// TODO: USE THIS PROPERLY. THE FUNCTION AS IS WORKS.
+function getWeatherForecast(cityName) {
+  let request = `${BASE_URL}/current.json?key=${API_KEY}&q=${cityName}`;
+  fetch(request)
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((err) => console.error(err));
+}
